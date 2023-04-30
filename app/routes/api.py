1dc4a3a655fe6d9e075bde885aaab066eb0402a4
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-30 16:39:24 -0700

    Error handling added with system print to view and test all except cases.

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
@@ -6,16 +7,20 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 
 @bp.route('/users', methods=['POST'])
 def signup():
-    data = request.get_json()
-    db = get_db()
+  data = request.get_json()
+  db = get_db()
 
+  try:
     newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
     )
 
     db.add(newUser)
     db.commit()
+  except:
+    print(sys.exe_info()[0])
+    return jsonify(message = 'Signup failed'), 500
 
-    return jsonify(id = newUser.id)
+  return jsonify(id = newUser.id)
