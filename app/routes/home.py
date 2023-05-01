0e58be00da888688f0b4e66429d8a4b95564cf5b
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-30 17:55:45 -0700

    Finishing home routes with redirection based on session logged in status.

--- app/routes/home.py
+++ app/routes/home.py
@@ -16,7 +16,10 @@ def index():
 
 @bp.route('/login')
 def login():
-  return render_template('login.html')
+  if session.get('loggedIn') is None:
+    return render_template('login.html')
+  
+  return redirect('/dashboard')
 
 @bp.route('/post/<id>')
 def single(id):
